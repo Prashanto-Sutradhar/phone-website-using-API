0bfbd4cac4da73ac45bcdc300e89ba63d7fc4ec7
prashanto777 <shishirsd777@gmail.com>
2023-09-01 04:05:11 +0600

    search option done

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-const loadPhone = async() => {
+const loadPhone = async(searchText) => {
 
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    console.log(phones)
+    // console.log(phones)
+    displayPhone(phones);
 }
-loadPhone();
+
+const displayPhone = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    //clear container  before search new phone
+    phoneContainer.textContent = '';
+
+    phones.forEach(phone => {
+        console.log(phone);
+        //create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        // set innerHtml
+
+        phoneCard.innerHTML = `<figure><img src="${phone.image}" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>`
+            //append child
+        phoneContainer.appendChild(phoneCard)
+    })
+}
+
+//handle search button
+
+
+const handleSearch = () => {
+    // console.log('search handle')
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText)
+    loadPhone(searchText);
+}
